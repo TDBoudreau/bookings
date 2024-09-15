783079c7d738bca27790c665a03b12d29b3f6da3
TDBoudreau <timothy.boudreau3@gmail.com>
2024-09-15 13:44:11 -0500

    Initial commit

---
+++ pkg/models/templatedata.go
@@ -0,0 +1,15 @@
+package models
+
+// TemplateData holds data sent from handlers to templates
+type TemplateData struct {
+	StringMap map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float64
+	Data      map[string]any
+	CSRFToken string
+	Flash     string
+	Warning   string
+	Error     string
+	// Authenticated bool
+	// Now           time.Time
+}
